b3815272867127139136f345c34047101eb5e9bc
mrmhxx82 <learingcreateantispambot@gmail.com>
2016-06-14 19:31:47 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,12 +230,9 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks",
-	"infoen",
-
-
+	"msg_checks"
     },
-    sudo_users = {190840223},--Sudo users
+    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
